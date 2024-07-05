1c926d0f55573e27f2968636e9bb27df350aa361
j0yglvdy5 <joygladys.njeri@student.moringaschool.com>
2024-07-05 09:32:57 +0300

    borrower info

--- lib/db/migrations/env.py
+++ lib/db/migrations/env.py
@@ -18,7 +18,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from db.models import Base  # Import the Base from the correct module
+from lib.models import Base  # Import the Base from the correct module
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
